be136c370e848ea6f5aa8217a69dc91550883b73
nico567765 <nicholashorton7@protonmail.com>
2023-06-28 14:13:51 +0100

    Finished initial Twenty-One

--- lesson3/twenty_one.rb
+++ lesson3/twenty_one.rb
@@ -1,14 +1,16 @@
 # Twenty-One
 
-UI = {
+TEXT = {
   greeting: "Welcome to Twenty-One!",
   dealer_has: "Dealer has: ",
   player_has: "You have: ",
   hit_stay: "Hit or stay?",
+  dealers_turn: "Dealer's turn",
   player_bust: "You're bust!",
   dealer_but: "Dealer is bust!",
   you_won: "You won!",
   dealer_won: "Dealer won!",
+  again: "Play again ('y'/'n')?",
   sign_off: "Thanks for playing Twenty-One, good bye!"
 }
 VALID_MOVES = ['hit', 'h', 'stay', 's']
@@ -69,41 +71,78 @@ def show_one_card(hand, message)
   prompt "#{message}#{hand.first.first} and #{hand.size-1} unknown #{single_plural}"
 end
 
+def declare_winner(player_hand, dealer_hand)
+  if bust?(player_hand)
+    prompt "You're busted! Dealer wins!"
+  elsif bust?(dealer_hand)
+    prompt "Dealer is busted! You win!"
+  elsif score(player_hand) > score(dealer_hand)
+    prompt "You win!"
+  elsif score(dealer_hand) > score(player_hand)
+    prompt "Dealer wins!"
+  else
+    prompt "It's a tie!"
+  end
+end
+
 # *** Main ***
 system('clear')
-
-prompt(UI[:greeting])
+puts "*** #{TEXT[:greeting]} ***"
+system('sleep 1.6')
+system('clear')
 
 # # Main game loop
 loop do
-  
-  # Play loop
-  loop do
-    deck = initialize_deck
-    dealer_hand = []
-    deal!(deck, dealer_hand)
-    player_hand = []
-    deal!(deck, player_hand)
+  deck = initialize_deck
+  deck.shuffle!
+  player_hand = []
+  dealer_hand = []
+  deal!(deck, dealer_hand)
+  deal!(deck, player_hand)
 
-    # # Player turn loop
+  # Player's turn
+  loop do
+    system('clear')
+    show_one_card(dealer_hand, TEXT[:dealer_has])
+    show_hand(player_hand, TEXT[:player_has])
+    
+    move = nil
     loop do
-      move = nil
-      show_one_card(dealer_hand, UI[:dealer_has])
-      show_hand(player_hand, UI[:player_has])
-      
-      # Validate input loop
-      loop do
-        prompt(UI[:hit_stay])
-        move = gets.chomp.downcase
-        break if VALID_MOVES.include?(move)
-      end # Validate input loop
-      
-      break if VALID_MOVES[2..3].include?(move) || bust?(player_hand)
-    end # Player turn loop
-#     # Dealer turn loop
-#     loop do
-#     end # Dealer turn loop
-    break
-  end # Play loop
-  break
-end # Main game loop
+      prompt(TEXT[:hit_stay])
+      move = gets.chomp.downcase
+      break if VALID_MOVES.include?(move)
+    end
+
+    hit!(deck, player_hand) if move.start_with?('h')
+
+    break if bust?(player_hand) || move.start_with?('s')
+  end
+  
+  unless bust?(player_hand)
+    # Dealer's turn
+    system('clear')
+    print "=> #{TEXT[:dealers_turn]}."
+    3.times do |_|
+      system('sleep 0.7')
+      print '.'
+    end
+
+    loop do
+      break if score(dealer_hand) >= 17 || bust?(dealer_hand)
+      hit!(deck, dealer_hand)
+    end
+  end
+
+  system('clear')
+  show_hand(dealer_hand, TEXT[:dealer_has])
+  show_hand(player_hand, TEXT[:player_has])
+  declare_winner(player_hand, dealer_hand)
+
+  puts
+  prompt TEXT[:again]
+  play_again = gets.chomp 
+  break unless play_again.downcase.start_with?('y')
+end
+
+system('clear')
+prompt TEXT[:sign_off]
