21bff1eb05325f7e81e07e3eec4e881254d40403
nico567765 <nicholashorton7@protonmail.com>
2023-06-28 20:52:06 +0100

    More refactoring of Twenty-One

--- lesson3/twenty_one.rb
+++ lesson3/twenty_one.rb
@@ -1,47 +1,69 @@
 # Twenty-One
 
 TEXT = {
+  clubs: "Clubs",
+  spades: "Spades",
+  hearts: "Hearts",
+  diamonds: "Diamonds",
+  jack: "Jack",
+  queen: "Queen",
+  king: "King",
+  ace: "Ace",
+  hit: "hit",
+  stay: "stay",
+  card_value_map: "%s of %s",
+  full_hand: "%s%s and %s for a total of %d",
+  one_card: "Dealer has: %s of %s and %d unknown %s",
+  card: "card",
+  cards: "cards",
   greeting: "Welcome to Twenty-One!",
   dealer_has: "Dealer has: ",
   player_has: "You have: ",
-  hit_stay: "Hit or stay?",
+  hit_stay: "Do you want to (h)it or (s)tay?",
+  players_turn: "Player's turn",
   dealers_turn: "Dealer's turn",
-  player_bust: "You're bust!",
-  dealer_but: "Dealer is bust!",
-  you_won: "You won!",
+  dealer_hits: "Dealer hits!",
+  dealer_stays: "Dealer stays!",
+  final_score: "Final score",
+  player_bust: "You're bust! Dealer wins!",
+  dealer_bust: "Dealer is bust! You win!",
+  player_won: "You won!",
   dealer_won: "Dealer won!",
-  again: "Play again ('y'/'n')?",
+  tie: "It's a tie!",
+  again: "Play again (y / n)?",
   sign_off: "Thanks for playing Twenty-One, good bye!"
 }
-VALID_MOVES = ['hit', 'h', 'stay', 's']
 
-SUITS = ["Clubs", "Spades", "Hearts", "Diamonds"]
-FACE_CARDS = ["Jack", "King", "Queen"]
+VALID_MOVES = [TEXT[:hit], TEXT[:hit][0], TEXT[:stay], TEXT[:stay][0]]
+
+SUITS = [TEXT[:clubs], TEXT[:spades], TEXT[:hearts], TEXT[:diamonds]]
+FACE_CARDS = [TEXT[:jack], TEXT[:queen], TEXT[:king]]
 
 def prompt(message)
   puts "=> #{message}"
 end
 
 def initialize_deck
-  SUITS.each_with_object([]) do |suit, new_deck|
-    2.upto(10) { |value| new_deck << [value, suit] }
-    FACE_CARDS.each { |face_card| new_deck << [face_card, suit] }
-    new_deck << ["Ace", suit]
-  end
+  ([*(2..10)] + FACE_CARDS + [TEXT[:ace]]).product(SUITS).shuffle
 end
 
-def deal!(deck, hand)
-  2.times { |_| hand << deck.pop }
+def deal!(deck, player_hand, dealer_hand)
+  2.times do
+    player_hand << deck.pop
+    dealer_hand << deck.pop
+  end
+  return
 end
 
 def hit!(deck, hand)
   hand << deck.pop
+  return
 end
 
 def card_value(card)
   case
   when FACE_CARDS.include?(card.first) then 10
-  when card.first == "Ace"             then 1
+  when card.first == TEXT[:ace]        then 1
   else                                      card.first
   end
 end
@@ -49,7 +71,7 @@ end
 def score(hand)
   result = hand.reduce(0) { |acc, card| acc + card_value(card) }
   hand.each do |card|
-    if card.first == "Ace" && (result + 10) <= 21
+    if card.first == TEXT[:ace] && (result + 10) <= 21
       result += 10
       break
     end
@@ -61,28 +83,38 @@ def bust?(hand)
   score(hand) > 21
 end
 
-def show_hand(hand, message)
-  card_values = hand.map { |card| card.first }
-  prompt "#{message}#{card_values[0..-2].join(', ')} and #{card_values.last}"
-end
-
-def show_one_card(hand, message)
-  single_plural = hand.size > 2 ? "cards" : "card"
-  prompt "#{message}#{hand.first.first} and #{hand.size-1} unknown #{single_plural}"
-end
-
 def calculate_result(player_hand, dealer_hand)
-  if bust?(player_hand)
-    "You're busted! Dealer wins!"
-  elsif bust?(dealer_hand)
-    "Dealer is busted! You win!"
-  elsif score(player_hand) > score(dealer_hand)
-    "You win!"
-  elsif score(dealer_hand) > score(player_hand)
-    "Dealer wins!"
-  else
-    "It's a tie!"
+  case
+  when bust?(player_hand)                      then :player_bust
+  when bust?(dealer_hand)                      then :dealer_bust
+  when score(player_hand) > score(dealer_hand) then :player_won
+  when score(dealer_hand) > score(player_hand) then :dealer_won
+  else                                              :tie
   end
+end
+
+def show_hand(hand, whose_hand)
+  card_values = hand.map do |card|
+    format(TEXT[:card_value_map], card[0].to_s, card[1].to_s)
+  end
+  prompt format(TEXT[:full_hand], whose_hand, card_values[0..-2].join(', '), 
+                card_values.last.to_s, score(hand))
+end
+
+def show_one_card(hand)
+  single_plural = hand.size > 2 ? TEXT[:cards] : TEXT[:card]
+  prompt format(TEXT[:one_card], hand[0][0].to_s, hand[0][1].to_s,
+                hand.size-1, single_plural)
+end
+
+def dealer_turn_animation
+  system('clear')
+  print "=> #{TEXT[:dealers_turn]}."
+  3.times do
+    system('sleep 0.5')
+    print '.'
+  end
+  return
 end
 
 # *** Main ***
@@ -97,13 +129,13 @@ loop do
   deck.shuffle!
   player_hand = []
   dealer_hand = []
-  deal!(deck, dealer_hand)
-  deal!(deck, player_hand)
+  deal!(deck, player_hand, dealer_hand)
 
   # Player's turn
   loop do
     system('clear')
-    show_one_card(dealer_hand, TEXT[:dealer_has])
+    prompt TEXT[:players_turn]
+    show_one_card(dealer_hand)
     show_hand(player_hand, TEXT[:player_has])
     
     move = nil
@@ -117,26 +149,32 @@ loop do
 
     break if bust?(player_hand) || move.start_with?('s')
   end
-  
-  unless bust?(player_hand)
-    # Dealer's turn animation
-    system('clear')
-    print "=> #{TEXT[:dealers_turn]}."
-    3.times do |_|
-      system('sleep 0.7')
-      print '.'
-    end
 
+# TODO: make it display dealer's turn etc. even if dealer stays
+  unless bust?(player_hand)
+    system('clear')
+    prompt TEXT[:dealers_turn]
+    show_one_card(dealer_hand)
+    show_hand(player_hand, TEXT[:player_has])
+    system('sleep 1.6')
     loop do
       break if score(dealer_hand) >= 17 || bust?(dealer_hand)
       hit!(deck, dealer_hand)
+      prompt TEXT[:dealer_hits]
+      system('sleep 1.6')
     end
   end
 
+  unless bust?(dealer_hand)
+    prompt TEXT[:dealer_stays]
+    system('sleep 1.6')
+  end
+
   system('clear')
+  prompt TEXT[:final_score]
   show_hand(dealer_hand, TEXT[:dealer_has])
   show_hand(player_hand, TEXT[:player_has])
-  prompt calculate_result(player_hand, dealer_hand)
+  prompt TEXT[calculate_result(player_hand, dealer_hand)]
 
   puts
   prompt TEXT[:again]
