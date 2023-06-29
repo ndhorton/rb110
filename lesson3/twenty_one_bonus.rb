0891e62179ea922ab3f224bd8ec165d193605327
nico567765 <nicholashorton7@protonmail.com>
2023-06-29 20:08:20 +0100

    Add bonus features 1,2 to Twenty-One Bonus

--- lesson3/twenty_one_bonus.rb
+++ lesson3/twenty_one_bonus.rb
@@ -1,181 +1,170 @@
-# Twenty-One
+# Twenty-One Bonus Features
 
-TEXT = {
-  clubs: "Clubs",
-  spades: "Spades",
-  hearts: "Hearts",
-  diamonds: "Diamonds",
-  jack: "Jack",
-  queen: "Queen",
-  king: "King",
-  ace: "Ace",
-  hit: "hit",
-  stay: "stay",
-  card_value_map: "%s of %s",
-  full_hand: "%s%s and %s for a total of %d",
-  one_card: "Dealer has: %s of %s and %d unknown %s",
-  card: "card",
-  cards: "cards",
-  greeting: "Welcome to Twenty-One!",
-  dealer_has: "Dealer has: ",
-  player_has: "You have: ",
-  hit_stay: "Do you want to (h)it or (s)tay?",
-  players_turn: "Player's turn",
-  dealers_turn: "Dealer's turn",
-  dealer_hits: "Dealer hits!",
-  dealer_stays: "Dealer stays!",
-  final_score: "Final score",
-  player_bust: "You're bust! Dealer wins!",
-  dealer_bust: "Dealer is bust! You win!",
-  player_won: "You won!",
-  dealer_won: "Dealer won!",
-  tie: "It's a tie!",
-  again: "Play again (y / n)?",
-  sign_off: "Thanks for playing Twenty-One, good bye!"
-}
+# Question 2: The context of the first two calls to `play_again?` requires the possibilty of
+# skipping the rest of the current iteration. This is why the ternary operator is used
+# with `next` as the control statement if `play_again?` returns `true`, otherwise `break`.
+# The third  call to `play_again?` is at the end of the `loop` block, so it only requires
+# the option to `break` if `play_again?` returns `false`. There is no more code 
+# in the game loop, so if `play_again?` returns `true` then the next iteration will
+# begin.
 
-VALID_MOVES = [TEXT[:hit], TEXT[:hit][0], TEXT[:stay], TEXT[:stay][0]]
+SUITS = ['H', 'D', 'S', 'C']
+VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
 
-SUITS = [TEXT[:clubs], TEXT[:spades], TEXT[:hearts], TEXT[:diamonds]]
-FACE_CARDS = [TEXT[:jack], TEXT[:queen], TEXT[:king]]
-
-def prompt(message)
-  puts "=> #{message}"
+def prompt(msg)
+  puts "=> #{msg}"
 end
 
 def initialize_deck
-  ([*(2..10)] + FACE_CARDS + [TEXT[:ace]]).product(SUITS).shuffle
+  SUITS.product(VALUES).shuffle
 end
 
-def deal!(deck, player_hand, dealer_hand)
-  2.times do
-    player_hand << deck.pop
-    dealer_hand << deck.pop
-  end
-end
+def total(cards)
+  # cards = [['H', '3'], ['S', 'Q'], ... ]
+  values = cards.map { |card| card[1] }
 
-def hit!(deck, hand)
-  hand << deck.pop
-end
-
-def card_value(card)
-  if FACE_CARDS.include?(card.first) then 10
-  elsif card.first == TEXT[:ace]     then 1
-  else
-    card.first
-  end
-end
-
-def score(hand)
-  result = hand.reduce(0) { |acc, card| acc + card_value(card) }
-  hand.each do |card|
-    if card.first == TEXT[:ace] && (result + 10) <= 21
-      result += 10
-      break
+  sum = 0
+  values.each do |value|
+    if value == "A"
+      sum += 11
+    elsif value.to_i == 0
+      sum += 10
+    else
+      sum += value.to_i
     end
   end
-  result
+
+  # correct for Aces
+  values.select { |value| value == "A" }.count.times do
+    sum -= 10 if sum > 21
+  end
+
+  sum
 end
 
-def bust?(hand)
-  score(hand) > 21
+def busted?(total_value)
+  total_value > 21
 end
 
-def calculate_result(player_hand, dealer_hand)
-  if bust?(player_hand)                         then :player_bust
-  elsif bust?(dealer_hand)                      then :dealer_bust
-  elsif score(player_hand) > score(dealer_hand) then :player_won
-  elsif score(dealer_hand) > score(player_hand) then :dealer_won
+# :tie, :dealer, :player, :dealer_busted, :player_busted
+def detect_result(dealer_total, player_total)
+  if player_total > 21
+    :player_busted
+  elsif dealer_total > 21
+    :dealer_busted
+  elsif dealer_total < player_total
+    :player
+  elsif dealer_total > player_total
+    :dealer
   else
     :tie
   end
 end
 
-def show_hand(hand, whose_hand)
-  card_values = hand.map do |card|
-    format(TEXT[:card_value_map], card[0].to_s, card[1].to_s)
+def display_result(dealer_total, player_total)
+  result = detect_result(dealer_total, player_total)
+
+  case result
+  when :player_busted
+    prompt "You busted! Dealer wins!"
+  when :dealer_busted
+    prompt "Dealer busted! You win!"
+  when :player
+    prompt "You win!"
+  when :dealer
+    prompt "Dealer wins!"
+  when :tie
+    prompt "It's a tie!"
   end
-  prompt format(TEXT[:full_hand], whose_hand, card_values[0..-2].join(', '),
-                card_values.last.to_s, score(hand))
 end
 
-def show_one_card(hand)
-  single_plural = hand.size > 2 ? TEXT[:cards] : TEXT[:card]
-  prompt format(TEXT[:one_card], hand[0][0].to_s, hand[0][1].to_s,
-                hand.size - 1, single_plural)
+def play_again?
+  puts "-------------"
+  prompt "Do you want to play again? (y or n)"
+  answer = gets.chomp
+  answer.downcase.start_with?('y')
 end
 
-def show_results(player_hand, dealer_hand)
-  system('clear')
-  prompt TEXT[:final_score]
-  show_hand(dealer_hand, TEXT[:dealer_has])
-  show_hand(player_hand, TEXT[:player_has])
-  prompt TEXT[calculate_result(player_hand, dealer_hand)]
-  puts
-end
-
-# Main
-system('clear')
-puts "*** #{TEXT[:greeting]} ***"
-system('sleep 1.6')
-system('clear')
-
-# Main game loop
 loop do
+  prompt "Welcome to Twenty-One!"
+
+  # initialize vars
   deck = initialize_deck
-  deck.shuffle!
-  player_hand = []
-  dealer_hand = []
-  deal!(deck, player_hand, dealer_hand)
+  player_cards = []
+  dealer_cards = []
 
-  # Player's turn
+  # initial deal
+  2.times do
+    player_cards << deck.pop
+    dealer_cards << deck.pop
+  end
+
+  player_total = total(player_cards)
+  dealer_total = total(dealer_cards)
+
+  prompt "Dealer has #{dealer_cards[0]} and ?"
+  prompt "You have: #{player_cards[0]} and #{player_cards[1]}, for a total of #{player_total}."
+
+  # player turn
   loop do
-    system('clear')
-    prompt TEXT[:players_turn]
-    show_one_card(dealer_hand)
-    show_hand(player_hand, TEXT[:player_has])
-
-    move = nil
+    player_turn = nil
     loop do
-      prompt(TEXT[:hit_stay])
-      move = gets.chomp.downcase
-      break if VALID_MOVES.include?(move)
+      prompt "Would you like to (h)it or (s)tay?"
+      player_turn = gets.chomp.downcase
+      break if ['h', 's'].include?(player_turn)
+      prompt "Sorry, must enter 'h' or 's'."
     end
 
-    hit!(deck, player_hand) if move.start_with?('h')
+    if player_turn == 'h'
+      player_cards << deck.pop
+      player_total = total(player_cards)
+      prompt "You chose to hit!"
+      prompt "Your cards are now: #{player_cards}"
+      prompt "Your total is now: #{player_total}"
+    end
 
-    break if bust?(player_hand) || move.start_with?('s')
+    break if player_turn == 's' || busted?(player_total)
   end
 
-  if bust?(player_hand)
-    show_results(player_hand, dealer_hand)
-    prompt TEXT[:again]
-    play_again = gets.chomp
-    play_again.downcase.start_with?('y') ? next : break
+  if busted?(player_total)
+    display_result(dealer_total, player_total)
+    play_again? ? next : break
+  else
+    prompt "You stayed at #{player_total}"
   end
 
-  # Dealer's turn
-  system('clear')
-  prompt TEXT[:dealers_turn]
-  show_one_card(dealer_hand)
-  show_hand(player_hand, TEXT[:player_has])
-  system('sleep 1.6')
+  # dealer turn
+  prompt "Dealer turn..."
+
   loop do
-    break if score(dealer_hand) >= 17 || bust?(dealer_hand)
-    hit!(deck, dealer_hand)
-    prompt TEXT[:dealer_hits]
-    system('sleep 1.6')
-  end
-  unless bust?(dealer_hand)
-    prompt TEXT[:dealer_stays]
-    system('sleep 1.6')
+    break if dealer_total >= 17
+
+    prompt "Dealer hits!"
+    dealer_cards << deck.pop
+    dealer_total = total(dealer_cards)
+    prompt "Dealer's cards are now: #{dealer_cards}"
   end
 
-  show_results(player_hand, dealer_hand)
-  prompt TEXT[:again]
-  play_again = gets.chomp
-  break unless play_again.downcase.start_with?('y')
+  if busted?(dealer_total)
+    prompt "Dealer total is now: #{dealer_total}"
+    display_result(dealer_total, player_total)
+    play_again? ? next : break
+  else
+    prompt "Dealer stays at #{dealer_total}"
+  end
+
+  # both player and dealer stays - compare cards
+  puts "============="
+  prompt "Dealer has #{dealer_cards}, for a total of: #{dealer_total}"
+  prompt "Player has #{player_cards}, for a total of: #{player_total}"
+  puts "============="
+
+  display_result(dealer_total, player_total)
+
+  break unless play_again?
 end
 
-system('clear')
-prompt TEXT[:sign_off]
+prompt "Thank you for playing Twenty-One! Good bye!"
+
+
+
