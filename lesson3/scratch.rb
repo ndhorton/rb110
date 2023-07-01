fa969071663cbfcfe763d103575ea4121d6b3832
nico567765 <nicholashorton7@protonmail.com>
2023-07-01 18:45:09 +0100

    Add final Twenty-One Bonus Features

--- lesson3/scratch.rb
+++ lesson3/scratch.rb
@@ -1,6 +1,15 @@
-require 'yaml'
+def fizzbuzz(upper_limit)
+  divisible_by = -> divisor { -> m { m % divisor == 0 } }
 
-obj = YAML.load_file("/home/nicholas/launch_school/rb110/lesson3/scratch.yml")
-HSH = obj['english']
-puts format(HSH['str1'], "world")
-puts HSH['str2'] % ["my", "friend"]
+  (1..upper_limit).map do |n|
+    case n
+    when divisible_by[15] then puts "fizzbuzz"
+    when divisible_by[5]  then puts "buzz"
+    when divisible_by[3]  then puts "fizz"
+    else
+      puts n
+    end
+  end
+end
+
+fizzbuzz(50)
--- lesson3/twenty_one_bonus.rb
+++ lesson3/twenty_one_bonus.rb
@@ -1,15 +1,23 @@
 # Twenty-One Bonus Features
 
-# Question 2: The context of the first two calls to `play_again?` requires the possibilty of
-# skipping the rest of the current iteration. This is why the ternary operator is used
-# with `next` as the control statement if `play_again?` returns `true`, otherwise `break`.
-# The third  call to `play_again?` is at the end of the `loop` block, so it only requires
-# the option to `break` if `play_again?` returns `false`. There is no more code 
-# in the game loop, so if `play_again?` returns `true` then the next iteration will
-# begin.
+# Question 2: The context of the first two calls to `play_again?` requires the
+# possibilty of skipping the rest of the current iteration. This is why the
+# ternary operator is used, passing control to a `next` statement if
+# `play_again?` returns `true`.
+# The third  call to `play_again?` is at the end of the `loop` block, so it only
+# requires the option to `break` if `play_again?` returns `false`. There is no
+# more code in the game loop, so if `play_again?` returns `true` then the next
+# iteration will immediately begin without requiring `next` to skip over any
+# code.
 
 SUITS = ['H', 'D', 'S', 'C']
-VALUES = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+VALUES = [
+  '2', '3', '4', '5', '6', '7', '8',
+  '9', '10', 'J', 'Q', 'K', 'A'
+]
+ROUNDS_TO_WIN = 5
+MAX_POINTS = 21
+DEALER_STAYS = 17
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -25,32 +33,32 @@ def total(cards)
 
   sum = 0
   values.each do |value|
-    if value == "A"
-      sum += 11
-    elsif value.to_i == 0
-      sum += 10
-    else
-      sum += value.to_i
-    end
+    sum += if value == "A"
+             11
+           elsif value.to_i == 0
+             10
+           else
+             value.to_i
+           end
   end
 
   # correct for Aces
   values.select { |value| value == "A" }.count.times do
-    sum -= 10 if sum > 21
+    sum -= 10 if sum > MAX_POINTS
   end
 
   sum
 end
 
 def busted?(total_value)
-  total_value > 21
+  total_value > MAX_POINTS
 end
 
 # :tie, :dealer, :player, :dealer_busted, :player_busted
 def detect_result(dealer_total, player_total)
-  if player_total > 21
+  if player_total > MAX_POINTS
     :player_busted
-  elsif dealer_total > 21
+  elsif dealer_total > MAX_POINTS
     :dealer_busted
   elsif dealer_total < player_total
     :player
@@ -61,7 +69,14 @@ def detect_result(dealer_total, player_total)
   end
 end
 
-def display_result(dealer_total, player_total)
+def display_cards(dealer_cards, player_cards, dealer_total, player_total)
+  puts "============="
+  prompt "Dealer has #{dealer_cards}, for a total of: #{dealer_total}"
+  prompt "Player has #{player_cards}, for a total of: #{player_total}"
+  puts "============="
+end
+
+def display_round_result(dealer_total, player_total)
   result = detect_result(dealer_total, player_total)
 
   case result
@@ -78,6 +93,21 @@ def display_result(dealer_total, player_total)
   end
 end
 
+def display_game_result(dealer_wins, player_wins, round)
+  puts "============="
+  prompt "At the end of #{round} rounds:"
+  prompt "Dealer won #{dealer_wins} rounds"
+  prompt "Player won #{player_wins} rounds"
+  puts "============="
+  if dealer_wins > player_wins
+    prompt "Dealer was the first to win #{ROUNDS_TO_WIN} rounds. " \
+           "Dealer wins the game!"
+  else
+    prompt "You were the first to win #{ROUNDS_TO_WIN} rounds. " \
+           "You win the game!"
+  end
+end
+
 def play_again?
   puts "-------------"
   prompt "Do you want to play again? (y or n)"
@@ -85,8 +115,16 @@ def play_again?
   answer.downcase.start_with?('y')
 end
 
+system('clear')
+
+dealer_wins = 0
+player_wins = 0
+round = 0
+
 loop do
+  round += 1
   prompt "Welcome to Twenty-One!"
+  prompt "Round #{round}"
 
   # initialize vars
   deck = initialize_deck
@@ -103,7 +141,8 @@ loop do
   dealer_total = total(dealer_cards)
 
   prompt "Dealer has #{dealer_cards[0]} and ?"
-  prompt "You have: #{player_cards[0]} and #{player_cards[1]}, for a total of #{player_total}."
+  prompt "You have: #{player_cards[0]} and #{player_cards[1]}, " \
+         "for a total of #{player_total}."
 
   # player turn
   loop do
@@ -127,7 +166,15 @@ loop do
   end
 
   if busted?(player_total)
-    display_result(dealer_total, player_total)
+    dealer_wins += 1
+    display_cards(dealer_cards, player_cards, dealer_total, player_total)
+    display_round_result(dealer_total, player_total)
+    if dealer_wins == ROUNDS_TO_WIN || player_wins == ROUNDS_TO_WIN
+      display_game_result(dealer_wins, player_wins, round)
+      round = 0
+      player_wins = 0
+      dealer_wins = 0
+    end
     play_again? ? next : break
   else
     prompt "You stayed at #{player_total}"
@@ -137,34 +184,47 @@ loop do
   prompt "Dealer turn..."
 
   loop do
-    break if dealer_total >= 17
-
+    break if dealer_total >= DEALER_STAYS
     prompt "Dealer hits!"
     dealer_cards << deck.pop
     dealer_total = total(dealer_cards)
     prompt "Dealer's cards are now: #{dealer_cards}"
+    prompt "Dealer total is now: #{dealer_total}"
   end
 
   if busted?(dealer_total)
-    prompt "Dealer total is now: #{dealer_total}"
-    display_result(dealer_total, player_total)
+    player_wins += 1
+    display_cards(dealer_cards, player_cards, dealer_total, player_total)
+    display_round_result(dealer_total, player_total)
+    if dealer_wins == ROUNDS_TO_WIN || player_wins == ROUNDS_TO_WIN
+      display_game_result(dealer_wins, player_wins, round)
+      round = 0
+      player_wins = 0
+      dealer_wins = 0
+    end
     play_again? ? next : break
   else
     prompt "Dealer stays at #{dealer_total}"
   end
 
   # both player and dealer stays - compare cards
-  puts "============="
-  prompt "Dealer has #{dealer_cards}, for a total of: #{dealer_total}"
-  prompt "Player has #{player_cards}, for a total of: #{player_total}"
-  puts "============="
+  display_cards(dealer_cards, player_cards, dealer_total, player_total)
+  display_round_result(dealer_total, player_total)
 
-  display_result(dealer_total, player_total)
+  if dealer_total > player_total
+    dealer_wins += 1
+  elsif player_total > dealer_total
+    player_wins += 1
+  end
+
+  if dealer_wins == ROUNDS_TO_WIN || player_wins == ROUNDS_TO_WIN
+    display_game_result(dealer_wins, player_wins, round)
+    round = 0
+    player_wins = 0
+    dealer_wins = 0
+  end
 
   break unless play_again?
 end
 
 prompt "Thank you for playing Twenty-One! Good bye!"
-
-
-
